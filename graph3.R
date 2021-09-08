bf545d0bb637a43c9aafe81623919fdf56fa21e1
Kate <kcd6znz@virginia.edu>
2021-09-08 15:37:09 -0400

    titles

--- graph3.R
+++ graph3.R
@@ -3,6 +3,7 @@ library(tidyverse)
 arrests <- read_csv("./USArrests.csv")
 str(arrests)
 
-graph2 <- ggplot(arrests, aes(x=UrbanPop, y=Assault)) + geom_point()
+graph2 <- ggplot(arrests, aes(x=UrbanPop, y=Assault)) + geom_point() +
+  labs(title="arrests")
 graph2
 
